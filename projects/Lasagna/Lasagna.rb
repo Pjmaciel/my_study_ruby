e210897e992af696dcdfc3ba2be32fec0a60c0ac
Pjmaciel <pjmaciel_4@hotmail.com>
2024-01-26 14:47:43 -0300

    refactor: Fixed logical from show result

--- projects/Lasagna/Lasagna.rb
+++ projects/Lasagna/Lasagna.rb
@@ -1,25 +1,25 @@
 class Lasagna
+    EXPECTED_MINUTES_IN_OVEN = 40
   def remaining_minutes_in_oven(actual_minutes_in_oven)
-        result = actual_minutes_in_oven
+    result = EXPECTED_MINUTES_IN_OVEN -  actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
-        result = layers * 10
+    layers * 2
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
-    qtdLayers = preparation_time_in_minutes(number_of_layers)
+
+    qtd_layers = preparation_time_in_minutes(number_of_layers) 
     time = remaining_minutes_in_oven(actual_minutes_in_oven)
 
-    result = qtdLayers - time 
-
-    putTs = result > 1 ? "s": ""
-    putLs = qtdLayer > 1 ? "s": ""
-
-    puts " faltam #{result} minuto#{putTs} Para uma lasanha de #{qtdLayers} camada#{putLs}"
+    result = EXPECTED_MINUTES_IN_OVEN - (time - qtd_layers)
+    
+    puts "Faltam #{result} minutos para uma lasanha de #{qtd_layers} camadas"    
 
   end
 end
 
 lasanha = Lasagna.new
-lasanha.total_time_in_minutes(1,1)
+tempo_restante = lasanha.total_time_in_minutes(number_of_layers: 1, actual_minutes_in_oven: 30)
+
