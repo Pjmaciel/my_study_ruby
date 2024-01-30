2b0a2abcbbbcac9a7c97a0aac5e5c38c192517f6
Pjmaciel <pjmaciel_4@hotmail.com>
2024-01-30 14:33:35 -0300

    chore: implement logical from game

--- projects/oo/mastermindGame/gamer.rb
+++ projects/oo/mastermindGame/gamer.rb
@@ -1,11 +1,35 @@
 class Game
+    def initialize
+      @player = Player.new
+      @computer = Player.new
+      @max_turns = 12
+      @current_turn = 1 
+      @current_player = @players.first
+    end
     
     def play
-        loop do 
+        puts "Bem-vindo ao Mastermind!"
+
+        while @current_turn <= @max_turns
+            puts "\n [Turno #{current_turn}]"
+            
+
 
             break if game_over?
-        
+            current_turn++
+
             switch_player
         end
+
+        end_game
+    end
+    
+    private 
+
+    def game_over?
+
+    end
+
+    def end_game
     end
 end
