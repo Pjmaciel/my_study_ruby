e4ddf28c0c1fe4c4f9df9194dcb73f985f0153f3
Pjmaciel <pjmaciel_4@hotmail.com>
2024-01-30 16:48:41 -0300

    fix: fix game feedback to only display correct or incorrect

--- projects/oo/mastermindGame/gamer.rb
+++ projects/oo/mastermindGame/gamer.rb
@@ -1,35 +1,72 @@
+require_relative 'player'
+require 'securerandom'
+
 class Game
-    def initialize
-      @player = Player.new
-      @computer = Player.new
-      @max_turns = 12
-      @current_turn = 1 
-      @current_player = @players.first
+  attr_accessor :player, :secret_code, :turns
+
+  def initialize(player)
+    @player = player
+    @secret_code = []
+    @turns = 12
+  end
+
+  def start
+    puts "\nBem-vindo ao Mastermind!"
+    puts "Você tem 12 turnos para adivinhar o código secreto."
+
+    player.make_sequence
+    generate_secret_code
+
+    play_turn until game_over?
+
+    display_result
+  end
+
+  private
+
+  def generate_secret_code
+    4.times do 
+      @secret_code << SecureRandom.random_number(10) + 1
     end
-    
-    def play
-        puts "Bem-vindo ao Mastermind!"
+  end
 
-        while @current_turn <= @max_turns
-            puts "\n [Turno #{current_turn}]"
-            
+  def play_turn
+    player.make_guess
+    feedback = compare_guess
 
+    puts "\nFeedback: #{feedback.join(', ')}"
+    puts "Turnos restantes: #{@turns}"
 
-            break if game_over?
-            current_turn++
+    @turns -= 1
+  end
 
-            switch_player
+  def compare_guess
+    feedback = []
+    player.guess.each_with_index do |guess_digit, index|
+        if guess_digit == secret_code[index]
+            feedback << 'correto'
+        else
+             feedback << 'incorreto'
         end
-
-        end_game
-    end
-    
-    private 
-
-    def game_over?
-
     end
 
-    def end_game
+    feedback
+  end
+
+  def game_over?
+    turns.zero? || player.guess == secret_code
+  end
+
+  def display_result
+    if player.guess == secret_code
+      puts "\nParabéns! Você acertou o código secreto!"
+    else
+      puts "\nVocê perdeu! O código secreto era #{secret_code.join(', ')}"
     end
+  end
 end
+
+# Para iniciar o jogo:
+player1 = Player.new('Jogador1')
+game = Game.new(player1)
+game.start
